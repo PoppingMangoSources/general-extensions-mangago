252e72bb14557143111eed4e61c7096e9d899cee
PoppingMangoSources <ikjot97@hotmail.com>
2026-07-07 10:44:47 -0400

    Update pbconfig.ts

--- src/KingOfShojo/pbconfig.ts
+++ src/KingOfShojo/pbconfig.ts
@@ -6,7 +6,7 @@ import { ContentRating, SourceIntents, type ExtensionInfo } from "@paperback/typ
 export default {
   name: "KingOfShojo",
   description: "Extension that pulls content from kingofshojo.com.",
-  version: "1.0.0-alpha.8",
+  version: "1.0.0-alpha.9",
   icon: "icons.png",
   language: "en",
   contentRating: ContentRating.MATURE,
