46a7e6ca4ea246e9763778058ed8652af6cd113f
PoppingMangoSources <ikjot97@hotmail.com>
2026-07-09 12:34:30 -0400

    Update pbconfig.ts

--- src/OniSaga/pbconfig.ts
+++ src/OniSaga/pbconfig.ts
@@ -6,7 +6,7 @@ import { ContentRating, SourceIntents, type ExtensionInfo } from "@paperback/typ
 export default {
   name: "OniSaga",
   description: "Extension that pulls content from onisaga.com.",
-  version: "1.0.0-alpha.6",
+  version: "1.0.0-alpha.7",
   icon: "icon.png",
   language: "multi",
   contentRating: ContentRating.MATURE,
