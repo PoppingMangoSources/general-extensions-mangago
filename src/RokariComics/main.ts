76b6a0c5f1b07679c3c279d124e9ae1c750cdd4c
PoppingMangoSources <ikjot97@hotmail.com>
2026-07-13 05:08:59 -0400

    Update main.ts

--- src/RokariComics/main.ts
+++ src/RokariComics/main.ts
@@ -23,7 +23,7 @@ import { MangaStreamGeneric } from "../generic/main";
 import { type MangaStreamDiscoverSection, type MangaStreamSearchMetadata } from "../generic/models";
 import { getFilterTagsBySection, getIncludedTagBySection } from "../generic/utils";
 import pbconfig from "./pbconfig";
-import { getBaseUrlOverride, RokariComicsSettings } from "./forms";
+import { getBaseUrlOverride, RokariComicsSettings } from "./settings";
 
 const DOMAIN_NAME = "https://rokaricomics.com";
 
