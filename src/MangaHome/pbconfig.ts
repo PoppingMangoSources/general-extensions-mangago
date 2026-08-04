752efa4dbd9ad69c9d5d537c467e9fe9fdbe6cf3
PoppingMangoSources <297959108+PoppingMangoSources@users.noreply.github.com>
2026-08-04 11:19:39 -0400

    chore(MangaHome): bump version

--- src/MangaHome/pbconfig.ts
+++ src/MangaHome/pbconfig.ts
@@ -6,7 +6,7 @@ import { ContentRating, SourceIntents, type ExtensionInfo } from "@paperback/typ
 export default {
   name: "MangaHome",
   description: "Extension that pulls content from mangahome.com.",
-  version: "1.0.0-alpha.6",
+  version: "1.0.0-alpha.7",
   icon: "icon.png",
   language: "en",
   contentRating: ContentRating.MATURE,
